45b31c6b0016187a5934a0300a38fd44b28ff3a2
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2022-03-27 23:41:19 +0300

    user

--- games/arma3/entrypoint.sh
+++ games/arma3/entrypoint.sh
@@ -10,7 +10,7 @@
 STEAMCMD_DIR="./steamcmd"                       # SteamCMD's directory containing steamcmd.sh
 STEAMCMD_LOG="${STEAMCMD_DIR}/steamcmd.log"     # Log file for SteamCMD
 GAME_ID=107410                                  # SteamCMD ID for the Arma 3 GAME (not server). Only used for Workshop mod downloads.
-EGG_URL='https://github.com/parkervcp/eggs/tree/master/game_eggs/steamcmd_servers/arma/arma3'   # URL for Pterodactyl Egg & Info (only used as info to legacy users)
+EGG_URL='https://github.com/egomaw/eggs/tree/master/game_eggs/steamcmd_servers/arma/arma3'   # URL for Pterodactyl Egg & Info (only used as info to legacy users)
 
 # Color Codes
 CYAN='\033[0;36m'
