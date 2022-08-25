da0d93db7493390c76ec08176fbb2ed07fe611ea
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2022-08-25 22:15:23 +0300

    at

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -16,7 +16,7 @@ fi
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
