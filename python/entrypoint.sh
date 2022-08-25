b3e590582e759ae01d86380f5271d38554390456
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2022-08-25 20:10:18 +0300

    remove envsubst

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -10,7 +10,7 @@ python --version
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsubst)
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 # Take care of commands starting with an if block
 if [[ "$MODIFIED_STARTUP" == if* ]]; then
