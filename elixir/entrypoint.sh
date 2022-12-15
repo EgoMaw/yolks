bb96ad42ea9deb20727061da566796d5fa957671
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2022-12-15 17:56:37 +0200

    huh

--- elixir/entrypoint.sh
+++ elixir/entrypoint.sh
@@ -21,6 +21,8 @@ elixir --version
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
+mix deps.get
+
 # Display the command we're running in the output, and then execute it with the env
 # from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
