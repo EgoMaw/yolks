0b86f9a56f5110c05cd78ea1dd0b9592b78be218
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2023-04-04 23:00:42 +0300

    test

--- oses/debian/entrypoint.sh
+++ oses/debian/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes. 
+# Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsubst)
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+exec ${MODIFIED_STARTUP}
