c4b044e5a288d997fe4f9a8eba84d294b293ba66
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2023-04-05 04:20:16 +0300

    ffs

--- oses/test/entrypoint.sh
+++ oses/test/entrypoint.sh
@@ -5,9 +5,9 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 
 if [ -n "$PRE_STARTUP_SCRIPT" ]; then
-PRE_STARTUP_SCRIPT=$(echo "${PRE_STARTUP_SCRIPT}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsubst)
-
 echo -e "$PRE_STARTUP_SCRIPT"
+
+PRE_STARTUP_SCRIPT=$(echo "${PRE_STARTUP_SCRIPT}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsubst)
 
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mRunning Preflight Script...\n"
 eval "$PRE_STARTUP_SCRIPT"
