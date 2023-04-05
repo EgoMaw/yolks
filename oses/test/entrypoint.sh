52ffc7ec81cf82a8843dcd7503962db6c48719cf
EgoMaw <28892220+EgoMaw@users.noreply.github.com>
2023-04-05 05:06:06 +0300

    last one?

--- oses/test/entrypoint.sh
+++ oses/test/entrypoint.sh
@@ -4,15 +4,12 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 
+# Run Pre-flight script
 if [ -n "$PRE_STARTUP_SCRIPT" ]; then
-echo -e "$PRE_STARTUP_SCRIPT"
-
 PRE_STARTUP_SCRIPT=$(echo "${PRE_STARTUP_SCRIPT}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsubst)
-
-echo -e "$PRE_STARTUP_SCRIPT"
-
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mRunning Preflight Script...\n"
 eval "$PRE_STARTUP_SCRIPT"
+echo -e " "
 fi
 
 # Replace Startup Variables
@@ -20,4 +17,4 @@ MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | envsub
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval "$MODIFIED_STARTUP"
+exec $MODIFIED_STARTUP
